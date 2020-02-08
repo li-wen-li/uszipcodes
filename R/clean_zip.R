4d92520d83f247f3a2aa8dc3088c8d2159cefcfb
LinearFragility <55705409+li-wen-li@users.noreply.github.com>
2020-02-08 14:16:02 -0500

    clean_zip() updated

--- R/clean_zip.R
+++ R/clean_zip.R
@@ -5,11 +5,14 @@
 #'@return A character
 #'@export
 
+library(stringr)
 clean_zip <- function(x){
   x <- ifelse(contain_letter(x), "NA",
-              ifelse(stringr::str_detect(x, "  "), stringr::str_replace(x, "  ", "00"),
+              ifelse(stringr::str_detect(x, ", "), stringr::str_replace(x, ", ", "00"),
+                  ifelse(stringr::str_detect(x, "  "), stringr::str_replace(x, "  ", "00"),
                      ifelse(stringr::str_detect(x, " "), stringr::str_replace(x," ", "0"), stringr::str_pad(x, width = 5, pad = "0"))
-            )
+                     )
+                )
       )
   return(x)
 }
